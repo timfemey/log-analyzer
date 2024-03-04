b720c24684938c904ce43a56190e5af8a63a7e02
timfemey <isholaobafemi@gmail.com>
2024-03-04 16:56:37 +0100

    removed select cli prompt from code

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,6 @@
-import select from '@inquirer/select';
 import { input } from '@inquirer/prompts';
+import { readFile } from './controllers/readFiles.js';
 
+const fileName = await input({ message: 'Enter File Path to Log File: ' });
 
-
-const fileFormat = await select({
-    message: 'Select the file format of your log files',
-    choices: [
-        {
-            name: 'json',
-            value: 'json',
-            description: 'Data objects consisting of attribute–value pairs and arrays (or other serializable values), It is a commonly used data format. ',
-        },
-        {
-            name: 'text',
-            value: 'text',
-            description: 'A text file (sometimes spelled textfile; an old alternative name is flatfile) is a kind of computer file that is structured as a sequence of lines',
-        },
-
-    ],
-});
-
-
-const fileName = await input({ message: 'Enter Name of Log File: ' });
-
+const readFiles = readFile(fileName)
