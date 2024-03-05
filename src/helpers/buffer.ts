9d53508d75ce2e25324b6ad093b7cbd51e5d9ff8
timfemey <isholaobafemi@gmail.com>
2024-03-05 16:32:21 +0100

    removed unnecessary convert of Buffer to String Function

--- src/helpers/buffer.ts
+++
@@ -1,7 +0,0 @@
-export function bufferToString(data: string | Buffer) {
-    if (Buffer.isBuffer(data)) {
-        return data.toString('utf8'); // Convert buffer to string using utf8 encoding
-    } else {
-        return data; // Return as is if not a Buffer
-    }
-}
